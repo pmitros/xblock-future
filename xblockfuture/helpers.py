111af550334df923cfed8f29e195dfd1a211eda5
Piotr Mitros <pmitros@mitx.mit.edu>
2014-07-28 15:56:30 -0400

    Bug fixesx

--- xblockfuture/helpers.py
+++ xblockfuture/helpers.py
@@ -20,26 +20,29 @@ class Grade():
     def get_maxscore(self):
         return self._maxscore
 
-def futureclass(original_class, config={}):
+def futureclass(config={}):
     ''' Modify the class c to support future proposed features. 
     '''
-    # Step 1: Give it a useful name. 
+    def futureclass_helper(original_class):
+        # Step 1: Give it a useful name. 
+        config = {}
+        
+        if 'name' in config:
+            class_name = config['name']
+        elif hasattr(original_class, 'display_name'):
+            class_name = original_class.display_name
+        elif hasattr(original_class, 'name'):
+            class_name = original_class.name
+        else:
+            class_name = str(type(original_class))
+        original_class.display_name = String(default=class_name, 
+                                             scope=Scope.settings,
+                                             help="Display name")
 
-    if 'name' in config:
-        class_name = config['name']
-    elif hasattr(original_class, 'display_name'):
-        class_name = original_class.display_name
-    elif hasattr(original_class, 'name'):
-        class_name = original_class.name
-    else:
-        class_name = str(type(original_class))
-    original_class.display_name = String(default=class_name, 
-                                         scope=Scope.settings,
-                                         help="Display name")
+        # Step 2: Manage grading
+        if hasattr(original_class, "grade") and isinstance(original_class.grade, Grade):
+            original_class.has_score = True
+            original_class.max_score = lambda x: x.grade.get_maxscore()
 
-    # Step 2: Manage grading
-    if hasattr(original_class, grade) and isinstance(grade, Grade):
-        original_class.has_score = True
-        original_class.max_score = lambda x: x.grade.get_maxscore()
-
-    return original_class
+        return original_class
+    return futureclass_helper
